fb5b05d7b14246372e9a30a8b630947469094fff
Menno Luitjes <menno@mennoluitjes.nl>
2016-02-17 00:10:44 +0100

    Simplified documentation so it requires no editing when starting a new project, and minor documentation tweaks. Improved some function names to be more descriptive. Added the plugin version number to all classes.

--- includes/wpt_example.php
+++ includes/wpt_example.php
@@ -1,61 +1,55 @@
 <?php
 
-	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * The class that holds all main functionality of our extension.
+ *
+ * @since 0.1
+ */
+
+class WPT_Example {
+
+	function __construct() {
+
+		global $wpt_example_version;
+
+		// A unique identifier for our plugin, and its version.
+		$this->plugin_name = 'wpt_example';
+		$this->plugin_version = $wpt_example_version;
+
+		/*
+		 * Load the options for our plugin.
+		 * @see WPT_Example_Admin
+		 */
+		$this->options = get_option($this->plugin_name);
+	}
 
 	/**
-	 * WPT_Example class.
-	 *
-	 * This is the class that holds all functionality that is specific to your extension.
-	 * Be sure to rename all references to 'example'.
+	 * Loads and initializes the sub classes of our plugin.
 	 *
 	 * @since 0.1
+	 * @access private
+	 * @return void
 	 */
-	 
-	class WPT_Example {
-		
-		function __construct() {
-
-			// A unique identifier for your plugin.
-			$this->plugin_name = 'wpt_example';
-
-			/*
-			 * Load the options for your plugin.
-			 * @see WPT_Example_Admin
-			 */
-			$this->options = get_option($this->plugin_name);
-			
-			// Set more variables that you need to use throughout your extension below...
-			
+	private function load_dependencies() {
+		if (is_admin()) {
+			require_once(dirname(__FILE__) . '/wpt_example_admin.php');
+			$this->admin = new WPT_Example_Admin();
 		}
-		
-		/**
-		 * Loads and initializes the sub classes of your plugin.
-		 * 
-		 * @since 0.1
-		 * @access private
-		 * @return void
-		 */
-		private function load_dependencies() {
-			if (is_admin()) {
-				require_once(dirname(__FILE__) . '/wpt_example_admin.php');
-				$this->admin = new WPT_Example_Admin();
-			}			
-		}
-		
-		/**
-		 * Runs your extension.
-		 *
-		 * Use this to safely add your code when everything is fully loaded.
-		 * 
-		 * @return void
-		 */
-		public function run() {
-			
-			$this->load_dependencies();
-
-			// Start adding your stuff below...
-
-			
-		}
-		
 	}
+
+	/**
+	 * Runs our extension.
+	 *
+	 * Used to safely run our code when everything is fully loaded.
+	 *
+	 * @since 0.1
+	 * @access public
+	 * @return void
+	 */
+	public function run() {
+
+		$this->load_dependencies();
+	}
+}
--- loader.php
+++ loader.php
@@ -12,24 +12,26 @@ Domain Path: /languages
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+$wpt_example_version = '0.1';
+
 /**
- * Loads your extension.
+ * Loads our extension.
  *
  * Triggered by the `wpt_loaded` action, which is fired after the Theater for WordPress plugin is loaded.
  * @see includes/wpt_example.php
- * 
+ *
  * @since 1.0
  * @return void
  */
 function wpt_example_loader() {
-	
+
 	global $wp_theatre;
-	
-	require_once(dirname(__FILE__) . '/includes/wpt_example.php');	
+
+	require_once(dirname(__FILE__) . '/includes/wpt_example.php');
 
 	/**
-	 * Add an instance of your class to the global Theater object and then run it.
-	 * 
+	 * Add an instance of our class to the global Theater object and then run it.
+	 *
 	 * Requires Theater 0.9.4.
 	 */
 	$wp_theatre->example = new WPT_Example();
